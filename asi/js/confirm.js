ce5985c26c8f2eec6b30771e568e2a625b291e1a
ejosem91 <ejosem91@gmail.com>
2014-09-30 00:44:52 -0600

    ahi te va

--- asi/js/confirm.js
+++ asi/js/confirm.js
@@ -2,11 +2,16 @@
 function confirmacion(confirma) {
     if (confirm("Esta seguro que desea ir a Mantenimiento de "+ confirma ) == true) {
         if (confirma === 'banco') {
-    document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/mantenimiento/manBanco.html"
+    		document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/mantenimiento/manBanco.html"
         }else if (confirma === 'usuario'){
             document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/html/expediente.html"
         }else if (confirma === 'perfil'){
             document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/html/index_miembro.html"
+        }else if (confirma == 'alergias'){
+        	document.location.href= "http://localhost/asi2/ASI2-G1-E1-022014/asi/mantenimiento/alergias.html"
+        }else if (confirma == 'estado'){
+        	document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/mantenimiento/estado.html"	
         }
+
     }
 }
