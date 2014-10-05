779382f284ed8140f4ffc3b71749eed1b1857104
momo-ouroboros <oalmejia@hotmail.com>
2014-10-05 12:44:17 -0600

    ahi va departe de momo

---
+++ asi/grupo/clases/cRegistro.php
@@ -0,0 +1,96 @@
+<?php
+class Registro
+{
+	
+    //Constructor
+
+   function __construct() 
+
+	{
+        global $DATA;
+        
+        $this->DATA = $DATA;
+    }
+
+	function crear_registro($parametrosReg)
+    {
+    $sql="INSERT INTO persona (nomPer, apelPer, fechNacPer, telPer, celPer,duiPer,pasPer,fotPer,callPer,numCasPer,colPer,municipio_idMunic,fchaCreaPer)"
+                            . " values (?,?,?,?,?,?,?,?,?,?,?,?,?) ";
+    $save = $this->DATA->Execute($sql, $parametrosReg); 
+          if ($save){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function seleccionar_departamento()
+	{
+			$sql = "SELECT * FROM departamento ORDER BY idDep desc";
+  
+		$rs = $this->DATA->Execute($sql);
+				if ( $rs->RecordCount()) {
+				while(!$rs->EOF){
+					$id                 	    = $rs->fields['idDep'];
+					$info[$id]['idDep']		= $rs->fields['idDep'];
+					$info[$id]['nomDep'] 	= $rs->fields['nomDep'];
+		  		    $rs->MoveNext();
+				}
+				$rs->Close();
+				return $info;
+			} else {
+				return false;
+			}
+		}
+
+function seleccionar_municipio()
+	{
+		
+			$sql = "SELECT * FROM municipio where departamento_idDep= ".$_POST['idDep'];
+		$rs = $this->DATA->Execute($sql);
+				if ( $rs->RecordCount()) {
+				while(!$rs->EOF){
+					$id                 	    = $rs->fields['idMunic'];
+					$info[$id]['idMunic']		= $rs->fields['idMunic'];
+					$info[$id]['nomMunic'] 		= $rs->fields['nomMunic'];
+		  		    $rs->MoveNext();
+				}
+				$rs->Close();
+				return $info;
+			} else {
+				return false;
+			}
+			}
+			    
+function seleccionar_grupos()
+{
+	$sql= "SELECT g.*, m.nomMunic, d.nomDep FROM municipio AS m, departamento AS d , grupo AS g
+  WHERE m.departamento_idDep = d.idDep and g.municipio_idMunic=m.idMunic;" ;
+	$rs= $this-> DATA->Execute($sql);
+	if($rs->RecordCount()){
+		while(!$rs->EOF){
+			$id= $rs-> fields['numGrup'];
+			$info[$id]['numGrup']=$rs-> fields['numGrup'];
+			$info[$id]['nomGruo']=$rs-> fields['nomGruo'];
+			$info[$id]['lugReuGrup']=$rs-> fields['lugReuGrup'];
+			$info[$id]['diaReuGrup']=$rs->fields['diaReuGrup'];
+			$info[$id]['horaReuGrup']= $rs->fields['horaReuGrup'];
+			$info[$id]['nomDep']= $rs->fields['nomDep'];
+			$info[$id]['nomMunic']= $rs->fields['nomMunic'];
+			//$info[$id]['municipio_idMunic']=$rs->fields['municipio_idMunic'];
+			$rs->MoveNext();
+			}
+			$rs->Close();
+			return $info;
+	}
+	else{
+		return false;
+		}
+		}
+
+
+     
+} 
+
+
+?>
