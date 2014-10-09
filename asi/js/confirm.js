b008b349567bdbe75634f89551f6461c8466e016
ejosem91 <ejosem91@gmail.com>
2014-10-08 22:28:15 -0600

    ya no esta el inscrip

--- asi/js/confirm.js
+++ asi/js/confirm.js
@@ -2,15 +2,15 @@
 function confirmacion(confirma) {
     if (confirm("Esta seguro que desea ir a Mantenimiento de "+ confirma ) == true) {
         if (confirma === 'banco') {
-    		document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/mantenimiento/manBanco.html"
+    		document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/manBanco.html"
         }else if (confirma === 'usuario'){
-            document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/html/expediente.html"
+            document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/expediente.html"
         }else if (confirma === 'perfil'){
-            document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/html/index_miembro.html"
+            document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/index_miembro.html"
         }else if (confirma == 'alergias'){
-        	document.location.href= "http://localhost/asi2/ASI2-G1-E1-022014/asi/mantenimiento/alergias.html"
+        	document.location.href= "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/alergias.html"
         }else if (confirma == 'estado'){
-        	document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/mantenimiento/estado.html"	
+        	document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/estado.html"	
         }
 
     }
