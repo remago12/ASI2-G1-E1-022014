f03f11a6eab1932ad78bca2fc1037c6641d61795
ejosem91 <ejosem91@gmail.com>
2014-10-14 23:34:33 -0600

    ahi va ya lo del nav solo queda ir poniendo mas links eso ya no afectara el estilo

--- asi/views/inscripcion_grupo.tpl.php
+++ asi/views/inscripcion_grupo.tpl.php
@@ -15,35 +15,46 @@
   	<script type="text/javascript" src="js/mapa.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-default" role="navigation">
+  	<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+    <a class="navbar-brand tema" href="#">Scout</a>
+    
+    <a class="navbar-brand tema" href="#">El Salvador</a>
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <br>
+      <img id="logo1" src="img/ases1.jpg" class="img-responsive hidden-xs hidden-sm" alt="Responsive image">	
+      
+
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+      	<!--solo tienen que   copiar la siguiente linea para generar mas items -->
+        <li><a href="#">Link</a></li>
+        <img id="logo2" src="img/logo1.png" class="img-responsive hidden-xs hidden-sm" alt="Responsive image">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 	
-	<nav class="navbar navbar-default" role="navigation">
-		<div class="container-fluid">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		      	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		        	<span class="sr-only">Toggle navigation</span>
-		        	<span class="icon-bar"></span>
-		        	<span class="icon-bar"></span>
-		        	<span class="icon-bar"></span>
-		      	</button>
-		      	<img id="logo1" src="img/ases1.jpg" class="img-responsive hidden-xs hidden-sm" alt="Responsive image">
-		    	<a  class="navbar-brand" href="#"><h3>SCOUT</h3>El Salvador</a> 
-		    </div> 
-
-		    <br>
-		    <div class="navbar-collapse collapse">
-		    <ul class="nav navbar-nav navbar-right navbar">
-		    	<li><a href="indexadmin.html">Inicio</a></li>
-		       	<li><a href="solicitudes_de_miembro.html">Inscripciones</a></li>
-		    	<li><a href="#">Renovacion</a></li>
-		        <li><a href="#">Grupos Scout</a></li>
-		        <li><a href="#">Miembros Scout</a></li>
-		        <img id="logo2" src="img/logo1.png" class="img-responsive hidden-xs hidden-sm" alt="Responsive image">
-			</ul>
-			</div><!-- /.navbar-collapse -->
-
-		</div><!-- /.container-fluid -->
-	</nav>
 
 	<div class="container">
 		<form method="POST">
