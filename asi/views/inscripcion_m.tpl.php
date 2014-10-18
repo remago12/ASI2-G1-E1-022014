00ed4d8549a68d77961ed671901cfd24e01f6513
carloszc10 <carloszc85@hotmail.com>
2014-10-17 20:11:27 -0600

    views

--- asi/views/inscripcion_m.tpl.php
+++ asi/views/inscripcion_m.tpl.php
@@ -9,16 +9,16 @@
 <html>
 <head>
 <meta charset="UTF-8">
-	<title>Inscripcion de miembro</title>
-
+	<title>Inscripción de miembro</title>
 	<script type="text/javascript" src="../js/jquery-1.11.1.js"></script>
 	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="../css/custom.css">
+  	<link rel="stylesheet" type="text/css" href="../css/custom.css">
   	<link type="text/css" href="css/map.css" rel="stylesheet" media="all" />
-  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&language=es"></script>
-  <script type="text/javascript" src="../js/mapIns.js"></script>
-  <script type="text/javascript" src="../js/script_combo.js"></script>
+  	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&language=es"></script>
+  	<script type="text/javascript" src="../js/mapIns.js"></script>
+  	<script type="text/javascript" src="../js/script_combo.js"></script>
+  	<meta charset="UTF-8">
 </head>
 <body>
 	
@@ -26,12 +26,6 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
       <img id="logo1" src="../img/ases1.jpg" class="img-responsive" alt="Responsive image">
       <a  class="navbar-brand" href="#"><h3>SCOUT</h3>El Salvador</a> 
       
@@ -41,7 +35,7 @@
       <ul class="nav navbar-nav navbar-right">
         <li><a href="index.html">Inicio</a></li>
         <li><a href="#">Inscripciones</a></li>
-        <li><a href="#">Renovacion</a></li>
+        <li><a href="solRenoMiem">Renovación</a></li>
         <li><a href="#">Grupos Scout</a></li>
         <li><a href="#">Miembros Scout</a></li>
         <img id="logo2" src="../img/logo1.png" class="img-responsive" alt="Responsive image">
@@ -71,12 +65,12 @@
 			<label>Fecha de Nacimiento:</label>
 			<input type="date"   name="fecha" id="fecha" placeholder="Fecha" class="validate[required] medium form-control">
 			<br>
-			<label>Genero:</label>
+			<label>Género:</label>
 			<label>Masculino
 			<input type="radio" name="genero" checked="true">	
 			</label>
 			<label>Femenino
-			<input type="radio" name="genero"  >	
+			<input type="radio" name="genero">	
 			</label>
 			<div class="well well-lg">
 			<h2>Direccion</h2>
@@ -107,15 +101,15 @@
 			<br>
 			
 		<br>
-		<label>Telefono Casa:</label>
+		<label>Teléfono Casa:</label>
 		<input type="text"  name="telcasa" id="telcasa" placeholder="telcasa" class="validate[required] medium form-control"><br>
-		<label>Telefono Celular:</label>
+		<label>Teléfono Celular:</label>
 		<input type="text"  name="telcel" id="telcel" placeholder="telcel" class="validate[required] medium form-control"><br>
 		<label>Correo:</label>
 		<input type="email" name="email" id="email" placeholder="email" class="validate[required] medium form-control"><br>
 		<label>DUI:</label>
 		<input type="text"  name="dui" id="dui" placeholder="dui" class="validate[required] medium form-control"><br>
-		<label>Pasaporte:</label>scr
+		<label>Pasaporte:</label>
 		<input type="text"  name="pasaporte" id="pasaporte" placeholder="pasaporte" class="validate[required] medium form-control"><br>
 		<label>No. de grupo:</label><br>
 		<select class="combobox form-control" nombre="grupo" id="grupo">
--- asi/views/login.tpl.php
+++ asi/views/login.tpl.php
@@ -4,7 +4,7 @@
 <head>
 
 	<title>
-		
+	Login
 	</title>
   <script type="text/javascript" src="../js/jquery-1.11.1.js"></script>
   <script type="text/javascript" src="../js/bootstrap.min.js"></script>
@@ -13,19 +13,14 @@
   <link type="text/css" href="../css/mapLog.css" rel="stylesheet" media="all" />
   <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&language=es"></script>
   <script type="text/javascript" src="../js/mapLog.js"></script>
+  <meta charset="UTF-8">
 </head>
-<meta charset="UTF-8">
+
 <body>
   <nav class="navbar navbar-default log" role="navigation">
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <img id="logo1" src="../img/ases1.jpg" class="img-responsive" alt="Responsive image">
         <a  class="navbar-brand" href="#"><h3>SCOUT</h3>El Salvador</a>       
       </div>
@@ -37,7 +32,7 @@
         <li><a href="solicitudes_de_inscripcion.html">Inscripciones</a></li>
         <li><a href="solicitudes_renovacion.html">Renovacion</a></li>
         <li><a href="grupos_scout.html">Grupos Scout</a></li>-->
-        <li class="nohov"><a href="inscripcion_m.html"> <button class="btn btn-default">Registrate</button></a></li>
+        <li class="nohov"><a href="inscripcion_m.html"> <button class="btn btn-default">Regístrate</button></a></li>
         <li class="nohov"> <a href="" data-toggle="modal" data-target="#myModal"><button class="btn btn-default">Ingresar</button></a></li>
         <img id="logo2" src="../img/logo1.png" class="img-responsive" alt="Responsive image">
       </ul>
@@ -62,7 +57,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-            <h4 class="modal-title" id="myModalLabel">Ingresa</h4>
+            <h4 class="modal-title" id="myModalLabel">Iniciar Sesión</h4>
           </div>
           <div class="modal-body">
             <div class="row">
@@ -75,11 +70,12 @@
                   <label>
                     Contraseña:
                   </label>
-                  <input type="password" placeholder="contraseña" name="password" id="password" class="form-control" >
+                  <input type="password" placeholder="Contraseña" name="password" id="password" class="form-control" >
                   <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
                     <button type="button" value="Entrar" class="btn btn-primary">Entrar</button>
-                    <a href="inscripcion_m.html">Registrate</a>
+                    <a href="inscripcion_m.tpl.php">Regístrate</a>
+                    <p><p><a href="inscripcion_m.tpl.php">¿Has olvidado tu contraseña?</a>
                   </div>
                 </form>
               </div>  
