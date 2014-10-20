b35f46aa86e2c3a7380b7789330de10dc6ecfc6d
vrburgos <vrburgos@live.com>
2014-10-19 22:51:45 -0600

    Ya guarda

--- asi/controller/inscripcion_grupo.php
+++ asi/controller/inscripcion_grupo.php
@@ -11,7 +11,7 @@
 	$exclGrup			          =$_POST['exclusivo'];
 	$lugReuGrup		          =$_POST['lugarReunion'];
 	$proLugGrup		          =$_POST['propLugar'];
-	$fchaFundGrup	          =$_POST['fecFundacion'];
+	$fchaFundGrup	          =$_POST['fechFundacion'];
 	$lugNacGrup             =$_POST['LugarFundacion'];
 	$diaReuGrup		          =$_POST['dia_reu'];
 	$horaReuGrup	          =$_POST['horaReunion'];
@@ -23,9 +23,11 @@
 	$latGrup			          =$_POST['txt_lat'];
 	$lngGrup 			          =$_POST['txt_lng'];
 	$idEst					= 1;
-	$usuario_nomUsu         =$_SESSION['usuario'];
+	$usuario_nomUsu         ='admin';
 	$telefono               =$_POST['telefono'];
 
+
 	$reg=array($numGrup,$nomGruo,$exclGrup,$lugReuGrup,$proLugGrup,$fchaFundGrup,$lugNacGrup,$diaReuGrup,$horaReuGrup,$limMiemGrup,$callGrup,$numCasGrup,$colGrup,$municipio_idMunic,$latGrup,$lngGrup,$idEst,$usuario_nomUsu, $telefono); 
+    
     $grupo->crearGrupo($reg);
 	
