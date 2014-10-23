c67a7b6506454950ff834200addbb8f084e2a037
anthony7265 <tony.castellon14@gmail.com>
2014-10-22 19:37:12 -0600

    tony

--- asi/controller/inscripcion_miem.php
+++ asi/controller/inscripcion_miem.php
@@ -6,10 +6,10 @@
 
 
 //variables POST
-  $registro =new Registro();
+  $registro = new Registro();
 
-  $NIS       = $_POST['NIS'];
-  $nombre     =$_POST['nombre'];
+  $NIS          =$_POST['NIS'];
+  $nombre       =$_POST['nombre'];
   $apellido     =$_POST['apellido'];
   $fechaNac     =$_POST['fechaNac'];
   $genero       =$_POST['genero'];
@@ -18,39 +18,28 @@
   $email        =$_POST['email'];
   $dui          =$_POST['dui'];
   $pasaporte    =$_POST['pasaporte'];
-  //$img          =$_FILES['file']['name'];
-  //$imagen       =$_POST['file'];
-  $calle      =$_POST['calle'];
-  $casa       =$_POST['casa'];
+  //$img        =$_FILES['file']['name'];
+  //$imagen     =$_POST['file'];
+  $calle        =$_POST['calle'];
+  $casa         =$_POST['casa'];
   $colonia      =$_POST['colonia'];
-  $municipio    = $_POST['municipio'];
-  $fecha      =date('y-m-d');
-  $grupo      = $_POST['grupo'];
+  $municipio    =$_POST['municipio'];
+  $fecha        =date('y-m-d');
+  $grupo        =$_POST['grupo'];
 
  
-//try{
- 
+try{
 //arrays
- 
     $reg=array($nombre,$apellido,$fechaNac,$genero,$telcasa,$celular,$email,$dui,$pasaporte,$calle,$casa,$colonia,$municipio,$fecha); 
   $registro->crear_registro($reg);
-
 
   //$reg2=array($NIS,$NIS,2);
   //$registro->crear_usuario($reg2);
 
   $reg3= array($NIS,1,1,$grupo);
-  $registro->crear_inscripcion($reg3);
- 
+  $registro->crear_inscripcion($reg3); 
 
+  }catch(Exception $e){
 
-   //var_dump($reg);
-  
-  //header('Location: inscripcion_m.php ');  
-
-  // }catch(Exception $e){
-
-   //header('Location:  consulta.php'); 
-//echo "$e";
-    //var_dump($e);
-   //}
+   }
+   ?>
