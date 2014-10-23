d4f2811859fc1ff02167b6d3fd16bafb4f792b20
josen <e.josem91@gmail.com>
2014-10-23 00:13:17 -0600

    ahi va

---
+++ asi/js/indexefe.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".box").click(function(){
+    $( ".box" ).effect( "shake" );
+    document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/manBanco.html";
+  });
+  $(".box3").click(function(){
+    $( ".box3" ).effect( "shake" );
+    document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/manBanco.html";
+  });
+  $(".box4").click(function(){
+    $( ".box4" ).effect( "shake" );
+    document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/manBanco.html";
+  });
+  $(".box5").click(function(){
+    $( ".box5" ).effect( "shake" );
+    document.location.href = "http://localhost/asi2/ASI2-G1-E1-022014/asi/views/mantenimiento/manBanco.html";
+  });
+});
