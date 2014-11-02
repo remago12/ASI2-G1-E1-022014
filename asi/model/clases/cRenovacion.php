0278ef22ee92a168159747be9fc71c02fa2697f1
anthony7265 <tony.castellon14@gmail.com>
2014-11-02 10:40:51 -0600

    tony

---
+++ asi/model/clases/cRenovacion.php
@@ -0,0 +1,21 @@
+<?php 
+class Renovacion
+{
+	
+	function __construct()
+	{
+		global $DATA;
+		$this->DATA = $DATA;
+	}
+	function crear_renovacion($renovacion){
+		$sql="INSERT INTO renovacion(numSolRen,fchaRen,estado_idEst,exeRen,numFactRen,fchaPagRen,montoRen,banco_idBanc,miembro_nisMiem,grupo_idGrup,imgcomprobante)"
+		."values(?,?,?,?,?,?,?,?,?,?,?)";
+		$save = $this->DATA->Execute($sql, $renovacion);
+		if($save){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+?>
