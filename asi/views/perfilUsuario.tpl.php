6b812068adc542b2dff0f4d6e67e25454bb4e35d
josen <e.josem91@gmail.com>
2014-11-05 23:52:09 -0600

    ahi va

--- asi/views/perfilUsuario.tpl.php
+++ asi/views/perfilUsuario.tpl.php
@@ -302,9 +302,12 @@
         <br>
           <tr><td><?=$telgrup?></td></tr>      
         <br>
-				<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+				<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#contacto">
   				 Renovacion
 				</button>
+        <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+          Contacto de Emergencia
+        </button>
 			</div>
 		</div>	
 	</div>
@@ -354,6 +357,33 @@
       <div class="modal-footer">
         <button value="Entrar" class="btn btn-primary">Entrar</button>
       </form>
+          <!-- Modal -->
+<div class="modal fade" id="contacto" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Contactos</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <label>Nombre:</label>
+          <input type="text" name="nombre">
+          <label>Apellido:</label>
+          <input type="text" name="apellido">
+          <label>Parentesco:</label>
+          <input type="text" name="parentesco">
+
+        </form>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
       </div>
     </div>
   </div>
