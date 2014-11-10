3364e0e7544a57dd397f60ecd0c748f02948b9fa
momo-ouroboros <oalmejia@hotmail.com>
2014-11-09 21:25:54 -0600

    Mapa en modal listo

--- asi/js/grupos.js
+++ asi/js/grupos.js
@@ -8,6 +8,61 @@ $('#municipio').change(function(){
 grupos();
 });
 
+ $('#myModal').on('show.bs.modal', function(event) {
+        $("#txt_lat").val($(event.relatedTarget).data('latitud'));
+        $("#txt_lng").val($(event.relatedTarget).data('longitud'));
+
+    });
+
+  $('#myModal').on('shown.bs.modal', function(event) {
+ var lat = document.getElementById('txt_lat').value;
+ var lng = document.getElementById('txt_lng').value; 
+  var mapOptions = {
+    zoom: 16,
+    center: new google.maps.LatLng(lat,lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
+  var img = new google.maps.MarkerImage("../img/scouts.png");
+
+  var marker = new google.maps.Marker({
+    position: map.getCenter(),
+    map: map,
+    title: 'Click to zoom',
+    draggable:true,
+    icon:img
+
+  });
+
+  google.maps.event.addListener(marker, 'dragend', function() {
+
+      // Get the Current position, where the pointer was dropped
+
+      var point = marker.getPosition();
+
+      // Center the map at given point
+
+      map.panTo(point);
+
+      // Update the textbox
+
+      document.getElementById('txt_lat').value=point.lat();
+      document.getElementById('txt_lng').value=point.lng();
+   });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    map.setZoom(8);
+    map.setCenter(marker.getPosition());
+  });
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+        
+    });
+
+
 
 $("ul.paginacion").on("click","li", function(){
     // alert($(this).find("span.pag").text());
@@ -84,9 +139,10 @@ Municipio=data.rows[i]["7"];
 Lugarreu=data.rows[i]["3"];
 Diareu = data.rows[i]["4"];
 Horareu =data.rows[i]["5"];
+latitud= data.rows[i]["8"];
+longitud = data.rows[i]["9"];
 
-
-cadena= cadena +"<tr><td>"+Numgrup +"</td><td>"+Nomgrup +"</td><td>" + Departamento +"</td><td>"+Municipio+"</td><td>"+Lugarreu+"</td><td>"+Diareu+"</td><td>"+Horareu+"</td><td><a class='btn btn-primary btn-lg' data-toggle='modal' data-target='#myModal'>Mapa</a></td><td><a class='btn btn-primary btn-lg' href='modGrupo.php?IdGrup="+btoa(Idgrup)+"'>Editar</a></td></tr>";
+cadena= cadena +"<tr><td>"+Numgrup +"</td><td>"+Nomgrup +"</td><td>" + Departamento +"</td><td>"+Municipio+"</td><td>"+Lugarreu+"</td><td>"+Diareu+"</td><td>"+Horareu+"</td><td><a class='btn btn-primary btn-lg' data-toggle='modal' data-target='#myModal' data-latitud='"+latitud+"' data-longitud='"+longitud+"'>mapa</a></td><td><a class='btn btn-primary btn-lg' href='modGrupo.php?IdGrup="+btoa(Idgrup)+"'>Editar</a></td></tr>";
 $('#loop').html(cadena);
 } 
 for(p=1; p <= pags;p++){
@@ -96,5 +152,7 @@ $('ul.paginacion').html(paginacion)
 }
 },'json');
 },'json');
+
+
 
 }
