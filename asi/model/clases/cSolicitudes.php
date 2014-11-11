8973617141659cb743ebeb43794f5ce316b4efac
momo-ouroboros <oalmejia@hotmail.com>
2014-11-11 01:51:40 -0600

    falta renovacion

--- asi/model/clases/cSolicitudes.php
+++ asi/model/clases/cSolicitudes.php
@@ -7,6 +7,7 @@ class Solicitud
         
         $this->DATA = $DATA;
     }
+
  function seleccionar_inscripciones($IdGrup,$IdDep,$IdMun,$IdEst,$Inicio,$Limite){
 			$result = mysql_query("SELECT p.nomPer,p.apelPer,p.genPer,p.fechNacPer,d.nomDep ,m.nomMunic , i.numSolicInsc,i.estado_idEst,g.numGrup,g.nomGruo from persona as p, inscripcion as i, municipio as m , departamento as d, grupo as g where p.idPersona = i.persona_idPersona and p.municipio_idMunic = m.idMunic and m.departamento_idDep = d.idDep and i.grupo_idGrup = g.idGrup and m.departamento_idDep like '".$IdDep."' and p.municipio_idMunic like '".$IdMun."' and g.idGrup like '".$IdGrup."' and i.estado_idEst like '".$IdEst."'order by numSolicInsc asc limit ".$Inicio.",".$Limite."");
   $rows=array();
@@ -27,8 +28,7 @@ class Solicitud
     return array('rows'=>$rows);
 
     }
-  }
-
+  
   function seleccionar_renovaciones(){
       $result = mysql_query("SELECT r.numSolRen,mi.nisMiem,p.nomPer,p.apelPer,p.genPer,p.fechNacPer,d.nomDep,m.nomMunic,g.numGrup FROM renovacion as r, miembro as mi, persona as p,departamento as d,municipio as m,grupo as g where r.miembro_nisMiem = mi.nisMiem and mi.persona_idPersona=p.idPersona and p.municipio_idMunic=m.idMunic and m.departamento_idDep=d.idDep and mi.grupo_idGrup=g.idGrup and g.idGrup like '%' and d.idDep like '%' and m.idMunic like '%' ");
   $rows=array();
@@ -49,6 +49,9 @@ class Solicitud
     return array('rows'=>$rows);
 
     }
+
   }
+  
+
 
 ?>
