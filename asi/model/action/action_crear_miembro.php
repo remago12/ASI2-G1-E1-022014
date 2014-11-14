3ddd7ff9c375f28a16f5021936f698c4f8ede1ec
momo-ouroboros <oalmejia@hotmail.com>
2014-11-14 06:43:00 -0600

    hola

--- asi/model/action/action_crear_miembro.php
+++ asi/model/action/action_crear_miembro.php
@@ -12,20 +12,17 @@
  	$correoP = $_POST['correo'];
  	$idEstado = $_POST['estado_inscripcion'];
  	$NIS = $_POST['NIS'];
- 	$NIS= $NIS;
  	 echo $idI;
  	 echo "<br>";
- 	 echo $idEStado;
+ 	 echo $idEstado;
  	echo "<br>";
  	 echo $NIS;
-
-
 if ($idEstado == 4){
  	 $reg=array($NIS,$NIS,3);
  	  $registro->crear_usuario($reg); 
  	  $reg2=array($NIS,12,$NIS);
  	  $registro->crear_miembro($reg2,$idI);
- 	   $reg3=array($idEStado,$idI);
+ 	   $reg3=array($idEstado,$idI);
  	   $registro->estado_inscripcion($reg3);
  	   	$nombreCompleto = $nombre." ".$apellido;
     	$body = "Estimado ".$nombreCompleto." su solicitud ha sido aprobada puede ingresar al sistema con el usuario: ".$NIS." contraseña:".$NIS;
@@ -37,6 +34,13 @@ if ($idEstado == 4){
  		 	$nombreCompleto = $nombre." ".$apellido;
     	$body = "Estimado ".$nombreCompleto." ha realizado el pago de su inscripcion exitosamente";
     	$correo->enviarCorreo($correoP, $nombreCompleto, "Inscripcion aceptadas", $body );
+ 		
+ 	}elseif($idEstado == 6 ){
+ 		$reg1=array($idEstado,$idI);
+ 	   $registro->estado_inscripcion($reg1);
+ 		 	$nombreCompleto = $nombre." ".$apellido;
+    	$body = "Estimado ".$nombreCompleto." lo sentimos, pero su solicitud ha sido rechazada";
+    	$correo->enviarCorreo($correoP, $nombreCompleto, "Inscripcion Rechazada", $body );
  		
  	}
  	header('Location:../../controller/admin/solicitudes_inscripcion.php');
