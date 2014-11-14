985ca593341d0a4261cdca71a2dcad5f8d079489
momo-ouroboros <oalmejia@hotmail.com>
2014-11-14 06:39:02 -0600

    hola

--- asi/js/auto-gen.js
+++ asi/js/auto-gen.js
@@ -9,6 +9,7 @@ var apellido="";
 var num_SolicInsc=$('#idI').val();
 var d = new Date();
 var año = d.getFullYear().toString();
+var idI = $('#idI').val()
 
 $.post("../../model/clases/ajax.php",{action:"persona",num_SolicInsc:num_SolicInsc},function(data){
 	for(i= 0; i < data.rows.length;i++){
@@ -19,6 +20,7 @@ $.post("../../model/clases/ajax.php",{action:"corrnis"},function(data){
 if (data == null ){
 var NIS= nombre.substr(0,1) + apellido.substr(0,1) + "0001" +año.substr(2,4);
 		$('#NIS').val(NIS);
+		("hola");
 }else{
 for(i= 0; i< data.rows.length;i++){
 last_id=data.rows[i]["0"];
@@ -48,47 +50,4 @@ $('#NIS').val(NIS);
 },'json')
 
 });
-
-/*
-$('#apellido').keyup(function(){
-var nombre= $('#nombre').val().toUpperCase();
-var apellido= $('#apellido').val().toUpperCase();
-var last_id = "";
-var d = new Date();
-var año = d.getFullYear().toString();
-$.post("../model/clases/ajax.php",{action:"corrnis"},function(data){
-if (data == null){
-var NIS= nombre.substr(0,1) + apellido.substr(0,1) + "0001" +año.substr(2,4);
-$('#NIS').val(NIS);
-}else{
-for(i= 0; i < data.rows.length;i++){
-last_id=data.rows[i]["0"];
-if (last_id.length == 1){
-	var NIS= nombre.substr(0,1) + apellido.substr(0,1) + "000" + last_id + año.substr(2,4);
-$('#NIS').val(NIS);
-}
-else if (last_id.length==2){
-	var NIS= nombre.substr(0,1) + apellido.substr(0,1) + "00" + last_id + año.substr(2,4);
-$('#NIS').val(NIS);
-
-}
-else if (last_id.length==3){
-	var NIS= nombre.substr(0,1) + apellido.substr(0,1) + "0" + last_id + año.substr(2,4);
-$('#NIS').val(NIS);
-
-}
-else if (last_id.length==3){
-	var NIS= nombre.substr(0,1) + apellido.substr(0,1) + last_id + año.substr(2,4);
-$('#NIS').val(NIS);
-
-}
-}
-}},'json')
-//var cadena="<input type='text' name='NIS' id='NIS' placeholder='' class='validate[required] medium form-control'><br>";
-//$('#nisp').html(cadena); 
 });
-
-*/
-
-});
-
