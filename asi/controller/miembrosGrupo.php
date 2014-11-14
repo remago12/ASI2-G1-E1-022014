0050a5ab59fd9c2cfcc980edc727dee0bc0ae34d
vrburgos <vrburgos@live.com>
2014-11-14 03:19:46 -0600

    ahi le va

--- asi/controller/miembrosGrupo.php
+++ asi/controller/miembrosGrupo.php
@@ -1,8 +1,4 @@
 <?php
   require_once '../model/data/dataBase.php';
   require_once '../views/miembrosGrupo.tpl.php';
-<<<<<<< HEAD
-
-=======
->>>>>>> 4f4f7f8ec4d785cd93001d9057eda10ca842321f
   ?>
